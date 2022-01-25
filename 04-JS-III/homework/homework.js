2f741ff5da9cf78cf238821bfed87990048c3745
Facundo Gonzalez <fgonzalezdev@gmail.com>
2022-01-25 15:12:04 -0300

    4

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -148,9 +148,9 @@ function diaDeLaSemana(numeroDeDia) {
   //Escribe tu código aquí
   if(numeroDeDia == 1 || numeroDeDia == 7) {
     return "Es fin de semana";
+  } else {
+    return "Es dia Laboral"; 
   }
-  return "Es dia Laboral";
-  
 } 
 
 
